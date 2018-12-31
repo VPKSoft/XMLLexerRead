a6d7bf05130dc49ad001f7ef42d7c0fc0827f130
VPKSoft <vpksoft@vpksoft.net>
2018-12-31 14:47:22 +0200

    Initial file commit.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace XMLLexerRead
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.tbScintillaRead = new System.Windows.Forms.TextBox();
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.describeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cmbLanguage = new System.Windows.Forms.ComboBox();
+            this.odXML = new System.Windows.Forms.OpenFileDialog();
+            this.mnuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbScintillaRead
+            // 
+            this.tbScintillaRead.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbScintillaRead.Location = new System.Drawing.Point(12, 54);
+            this.tbScintillaRead.Multiline = true;
+            this.tbScintillaRead.Name = "tbScintillaRead";
+            this.tbScintillaRead.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tbScintillaRead.Size = new System.Drawing.Size(776, 384);
+            this.tbScintillaRead.TabIndex = 0;
+            this.tbScintillaRead.WordWrap = false;
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(800, 24);
+            this.mnuMain.TabIndex = 1;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuOpen,
+            this.describeToolStripMenuItem});
+            this.mnuFile.Image = global::XMLLexerRead.Properties.Resources.List;
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(53, 20);
+            this.mnuFile.Text = "File";
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Image = global::XMLLexerRead.Properties.Resources.Folder;
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(119, 22);
+            this.mnuOpen.Text = "Open";
+            this.mnuOpen.Click += new System.EventHandler(this.mnuOpen_Click);
+            // 
+            // describeToolStripMenuItem
+            // 
+            this.describeToolStripMenuItem.Image = global::XMLLexerRead.Properties.Resources.About;
+            this.describeToolStripMenuItem.Name = "describeToolStripMenuItem";
+            this.describeToolStripMenuItem.Size = new System.Drawing.Size(119, 22);
+            this.describeToolStripMenuItem.Text = "Describe";
+            this.describeToolStripMenuItem.Click += new System.EventHandler(this.describeToolStripMenuItem_Click);
+            // 
+            // cmbLanguage
+            // 
+            this.cmbLanguage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbLanguage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbLanguage.FormattingEnabled = true;
+            this.cmbLanguage.Location = new System.Drawing.Point(12, 27);
+            this.cmbLanguage.Name = "cmbLanguage";
+            this.cmbLanguage.Size = new System.Drawing.Size(776, 21);
+            this.cmbLanguage.TabIndex = 2;
+            this.cmbLanguage.SelectedIndexChanged += new System.EventHandler(this.cmbLanguage_SelectedIndexChanged);
+            // 
+            // odXML
+            // 
+            this.odXML.DefaultExt = "*.xml";
+            this.odXML.FileName = "stylers.model.xml";
+            this.odXML.Filter = "XML Files (*.xml)|*.xml";
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbLanguage);
+            this.Controls.Add(this.tbScintillaRead);
+            this.Controls.Add(this.mnuMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mnuMain;
+            this.Name = "FormMain";
+            this.Text = "XMLLexerRead";
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbScintillaRead;
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ComboBox cmbLanguage;
+        private System.Windows.Forms.OpenFileDialog odXML;
+        private System.Windows.Forms.ToolStripMenuItem describeToolStripMenuItem;
+    }
+}
+
---
+++ FormMain.cs
@@ -0,0 +1,100 @@
+﻿using ScintillaNET;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+
+namespace XMLLexerRead
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        XDocument document;
+        XDocument documentModel;
+
+        private void mnuOpen_Click(object sender, EventArgs e)
+        {
+            if (odXML.ShowDialog() == DialogResult.OK)
+            {
+                document = XDocument.Load(odXML.FileName);
+
+                documentModel = XDocument.Load(Path.Combine(Path.GetDirectoryName(odXML.FileName), "langs.model.xml"));
+
+                foreach (XElement element in document.Descendants("LexerType"))
+                {
+                    cmbLanguage.Items.Add(element.Attribute("name").Value);
+                }
+                cmbLanguage.SelectedIndex = 0;
+            }
+        }
+
+        private void cmbLanguage_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            tbScintillaRead.Clear();
+
+            tbScintillaRead.Text +=
+                "scintilla.StyleResetDefault();" + Environment.NewLine +
+                "scintilla.Styles[Style.Default].Font = \"Consolas\";" + Environment.NewLine +
+                "scintilla.Styles[Style.Default].Size = 10;" + Environment.NewLine +
+                "scintilla.StyleClearAll();" + Environment.NewLine + Environment.NewLine;
+
+            ComboBox comboBox = (ComboBox)sender;
+            string item = comboBox.SelectedItem.ToString();
+            var nodes = document.Descendants("LexerType").Where(f => f.Attribute("name").Value == item);
+            foreach (var node in nodes)
+            {
+                foreach (XElement element in node.Descendants("WordsStyle"))
+                {
+                    string fgColor = "#" + element.Attribute("fgColor").Value;
+                    string bgColor = "#" + element.Attribute("bgColor").Value;
+                    Color colorForeground = ColorTranslator.FromHtml(fgColor);
+                    Color colorBackground = ColorTranslator.FromHtml(bgColor);
+
+                    string fontStyle = "0";
+                    if (element.Attribute("fontStyle") != null)
+                    {
+                        fontStyle = element.Attribute("fontStyle").Value;
+                    }
+
+                    tbScintillaRead.Text +=
+                        $"// {element.Attribute("name").Value}, fontStyle = {fontStyle} " + Environment.NewLine +
+                    $"scintilla.Styles[{item}.{element.Attribute("name").Value}].ForeColor = Color.FromArgb({colorForeground.R}, {colorForeground.G}, {colorForeground.B}); // {fgColor} " + Environment.NewLine +
+                    $"scintilla.Styles[{item}.{element.Attribute("name").Value}].BackColor = Color.FromArgb({colorBackground.R}, {colorBackground.G}, {colorBackground.B}); // {bgColor} " + Environment.NewLine + Environment.NewLine;
+                }
+            }
+            tbScintillaRead.Text += "scintilla.Lexer = Lexer." + item + ";" + Environment.NewLine + Environment.NewLine;
+
+            nodes = documentModel.Descendants("Language").Where(f => f.Attribute("name").Value == item);
+
+            int keywordIndex = 0;
+            foreach(var node in nodes)
+            {
+                var subNodes = node.Descendants("Keywords");
+                foreach (var subNode in subNodes)
+                {
+                    tbScintillaRead.Text += "// Name: " + subNode.Attribute("name").Value + Environment.NewLine;
+                    tbScintillaRead.Text += $"scintilla.SetKeywords({keywordIndex++}, \"" + subNode.Value + "\");" + Environment.NewLine;
+                }
+            }
+        }
+
+        private void describeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Scintilla scintilla = new Scintilla();
+            scintilla.Lexer = Lexer.Cpp;
+            MessageBox.Show(scintilla.DescribeKeywordSets());
+        }
+    }
+}
