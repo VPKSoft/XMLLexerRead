511539996ddbfca12956e3e043c138fb3d5b6103
VPKSoft <vpksoft@vpksoft.net>
2020-12-17 19:04:38 +0200

    Updated project format.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,18 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("XMLLexerRead")]
-[assembly: AssemblyDescription("A code generator to create a lexer for the ScintillaNET from the famous Notepad++'s language definition files.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("VPKSoft")]
-[assembly: AssemblyProduct("XMLLexerRead")]
-[assembly: AssemblyCopyright("Copyright © VPKSoft 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
@@ -21,16 +9,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("71eb0f02-41a1-46aa-982c-3a7b1995871b")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
